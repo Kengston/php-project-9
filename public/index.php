947638ecbb244bbd45e0a422088bc265493fe2ae
Kengston <danidc@mail.ru>
2023-11-16 16:23:22 +0400

    php-view added

--- public/index.php
+++ public/index.php
@@ -3,12 +3,20 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
+use DI\Container;
 
+$container = new Container();
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
+$container->set('renderer', function () {
+    return new PhpRenderer(__DIR__ . "/../templates");
+});
+
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write("Hello, World!");
-    return $response;
+
+    return $this->get('renderer')->render($response, 'mainPage.phtml');
 });
 
 $app->run();
