8f83662ac75877175c6c56e07a10cb5fb492430b
Kengston <danidc@mail.ru>
2023-11-20 21:35:27 +0400

    version with DB

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use Slim\Views\PhpRenderer;
 use DI\Container;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Eloquent\Model;
 
 $container = new Container();
 AppFactory::setContainer($container);
@@ -14,9 +16,45 @@ $container->set('renderer', function () {
     return new PhpRenderer(__DIR__ . "/../templates");
 });
 
-$app->get('/', function ($request, $response) {
+$capsule = new Capsule;
+$capsule->addConnection([
+    'driver' => 'pgsql',
+    'host' => 'localhost',
+    'database' => 'database',
+    'username' => 'danillysikov',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+]);
 
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+class Url extends Model
+{
+    protected $table = 'urls';
+
+    protected $fillable = ['name'];
+}
+
+$app->get('/', function ($request, $response) {
     return $this->get('renderer')->render($response, 'mainPage.phtml');
+});
+
+$app->get('/urls', function ($request, $response) {
+
+
+    return $this->get('renderer')->render($response, 'allUrlsPage.phtml');
+});
+
+$app->post('/analyze', function ($request, $response) {
+    $urlName = $request->getParsedBody()['url'];
+
+    $url = new Url(['name' => $urlName]);
+    $url->save();
+
+    return $response->withHeader('Location', '/');
 });
 
 $app->run();
